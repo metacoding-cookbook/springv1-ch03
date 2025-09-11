74f8cc004c4ab01d78b397403c57d8f47d72a64a
coderyu <153602328+coderyu5523@users.noreply.github.com>
2025-09-11 21:35:01 +0900

    Update data.sql

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -1,8 +1,8 @@
 insert into user_tb (username,password,email,created_at) values ('ssar','1234','ssar@metacoding.com',now());
 insert into user_tb (username,password,email,created_at) values ('cos','1234','cos@metacoding.com',now());
 
-insert into board_tb (title, content, created_at,user_id) values ('title1', 'Spring Boot Study 1', now(),1); 
-insert into board_tb (title, content, created_at,user_id) values ('title2', 'Spring Boot Study 2', now(),1); 
+insert into board_tb (title, content, created_at,user_id) values ('title1', 'content1', now(),1); 
+insert into board_tb (title, content, created_at,user_id) values ('title2', 'content2', now(),1); 
 
 insert into reply_tb(comment,board_id,user_id,created_at) values('comment1',1,1,now());
 insert into reply_tb(comment,board_id,user_id,created_at) values('comment2',1,1,now());
