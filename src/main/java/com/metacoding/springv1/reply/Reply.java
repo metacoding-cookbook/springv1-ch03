e850bc1b8357939f4219cd2d4759c4595f25b886
coderyu5523 <coderyu5523@naver.com>
2025-09-12 22:25:44 +0900

    import 정리

--- src/main/java/com/metacoding/springv1/reply/Reply.java
+++ src/main/java/com/metacoding/springv1/reply/Reply.java
@@ -7,17 +7,8 @@ import org.hibernate.annotations.CreationTimestamp;
 import com.metacoding.springv1.board.Board;
 import com.metacoding.springv1.user.User;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import jakarta.persistence.*;
+import lombok.*;
 
 @NoArgsConstructor
 @Data
