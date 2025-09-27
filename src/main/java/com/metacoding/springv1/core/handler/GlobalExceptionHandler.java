7ee87a8cc350db0a52175efe814ea1d7cdd2645b
coderyu5523 <coderyu5523@naver.com>
2025-09-27 23:24:29 +0900

    import 정리

--- src/main/java/com/metacoding/springv1/core/handler/GlobalExceptionHandler.java
+++ src/main/java/com/metacoding/springv1/core/handler/GlobalExceptionHandler.java
@@ -1,4 +1,5 @@
 package com.metacoding.springv1.core.handler;
+
 import org.springframework.web.bind.annotation.*;
 import com.metacoding.springv1.core.util.Script;
 import com.metacoding.springv1.core.handler.ex.Exception401;
--- src/main/java/com/metacoding/springv1/reply/ReplyController.java
+++ src/main/java/com/metacoding/springv1/reply/ReplyController.java
@@ -1,12 +1,10 @@
 package com.metacoding.springv1.reply;
 
 import jakarta.servlet.http.HttpSession;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import com.metacoding.springv1.user.User;
 import com.metacoding.springv1.core.handler.ex.Exception401;
-
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
